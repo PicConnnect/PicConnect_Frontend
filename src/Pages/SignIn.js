0d76b322f245f4c8f7ca33ea7e3590cb80da3fa5
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-13 03:35:39 -0400

    feat: Add handler sign in function and add navigation back to profile

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,10 +1,21 @@
-import { signInWithGoogle } from "../firebase"
+import { signInWithGoogle } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-    return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={signInWithGoogle}>Sign in with Google</button>
-        </div>
-    )
+  const navigate = useNavigate();
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithGoogle(); //try to sign in with google
+      navigate("/profile"); // If sign in is successful, navigate to the profile page
+    } catch (error) {
+      console.error("Error signing in with Google", error);
+    }
+  };
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={handleSignIn}>Sign in with Google</button>
+    </div>
+  );
 }
