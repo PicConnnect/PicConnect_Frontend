90ebb8818d1dbbcf2e431c6e2bfff694b92ca807
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-13 03:11:42 -0400

    feat: Add observer for changes to the user's sign-in state

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 export default function Profile() {
-  return (
-    <div>This is Profile</div>
-  )
+  //initial state null (not logged in)
+  const [user, setUser] = useState(null);
+  //runs when the component mounts 
+  useEffect(() => {
+    //trigger when auth state changes (user logs in or out)
+    const unsubscribeFromAuthChange = auth.onAuthStateChanged(user => {
+      //set user state to new user (null if logged out)
+      setUser(user);
+    });
+    //cleanup (when component unmounts) to avoid memory leaks
+    return () => unsubscribeFromAuthChange();
+  }, [])
+
 }
