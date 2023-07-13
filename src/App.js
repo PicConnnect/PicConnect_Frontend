5a5afd589a235a97921df09d7a22a794d08054a9
woodley <gelinw3@gmail.com>
2023-07-12 22:59:40 -0400

    feat: user can navigate to Home, Following, Upload, Profile pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
-
+import './index.css';
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from "./Pages/Home";
+import Following from './Pages/Following';
+import Upload from './Pages/Upload';
+import Profile from './Pages/Profile';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+   <Router>
+      <div className="App">
+          <nav className="bg-[#C8DDD3] p-4">
+            <ul className="flex justify-between font-bold">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Following">Following</Link>
+              </li>
+              <li>
+                <Link to="/Upload">Upload</Link>
+              </li>
+              <li>
+                <Link to="/Profile">Profile</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/Following" element={<Following></Following>}></Route>
+            <Route path="/Upload" element={<Upload></Upload>}></Route>
+            <Route path="/Profile" element={<Profile></Profile>}></Route>
+          </Routes>
+          
+      </div>
+   </Router>
   );
 }
 
