f60bfd571db7b39f7682bb8f1630f1633efa635a
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-13 15:54:30 -0400

    feat: Add Email Address and Password form and copy over styling from sign up page

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,8 +1,20 @@
 import { signInWithGoogle, signInWithFacebook } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 export default function SignIn() {
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   const handleSignIn = async (signInMethod) => {
     try {
@@ -13,15 +25,65 @@ export default function SignIn() {
     }
   };
   return (
-    <div>
-      <h1>Sign In</h1>
-      <button onClick={() => handleSignIn(signInWithGoogle)}>
-        Sign in with Google
-      </button>
-      <br></br>
-      <button onClick={() => handleSignIn(signInWithFacebook)}>
-        Sign in with Facebook
-      </button>
+    <div className="bg-[#EAE6DE] h-screen">
+      <h1 className="text-4xl p-5">Login</h1>
+      <div className="flex justify-center">
+        <form className="flex flex-col w-1/2">
+
+          <div className="w-full pt-6">
+            <label for="email" className="block text-left font-bold">
+              Email Address
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+              className="w-full h-10"
+            />
+          </div>
+
+          <div className="w-full pt-6">
+            <label for="password" className="block text-left font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+              className="w-full h-10"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full h-10 mt-10 bg-black text-white"
+          >
+            Sign In
+          </button>
+
+          <button onClick={() => handleSignIn(signInWithGoogle)}>
+            Sign in with Google
+          </button>
+          <br></br>
+          <button onClick={() => handleSignIn(signInWithFacebook)}>
+            Sign in with Facebook
+          </button>
+
+          <span className="italic">
+            By continuing you agree to the{" "}
+            <a href="#" className="underline">
+              Terms of Services
+            </a>{" "}
+            and{" "}
+            <a href="#" className="underline">
+              {" "}
+              Privacy
+            </a>
+          </span>
+        </form>
+      </div>
     </div>
   );
 }
