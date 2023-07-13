6f325a647deb0bf6e32811d1509c3df386310fef
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-13 15:06:31 -0400

    feat: Add Sign in with facebook button

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,12 +1,12 @@
-import { signInWithGoogle } from "../firebase/firebase";
+import { signInWithGoogle, signInWithFacebook } from "../firebase/firebase";
 import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
   const navigate = useNavigate();
 
-  const handleSignIn = async () => {
+  const handleSignIn = async (signInMethod) => {
     try {
-      await signInWithGoogle(); //try to sign in with google
+      await signInMethod(); //try to sign in with google
       navigate("/profile"); // If sign in is successful, navigate to the profile page
     } catch (error) {
       console.error("Error signing in with Google", error);
@@ -15,7 +15,13 @@ export default function SignIn() {
   return (
     <div>
       <h1>Sign In</h1>
-      <button onClick={handleSignIn}>Sign in with Google</button>
+      <button onClick={() => handleSignIn(signInWithGoogle)}>
+        Sign in with Google
+      </button>
+      <br></br>
+      <button onClick={() => handleSignIn(signInWithFacebook)}>
+        Sign in with Facebook
+      </button>
     </div>
   );
 }
