043e7a8e9baa4658e4c817623c77f19a8c52d4a5
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-14 13:36:38 -0400

    fix: formatting

--- src/Pages/Upload.js
+++ src/Pages/Upload.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import UploadCard from '../components/UploadCard'
+import React from "react";
+import UploadCard from "../components/UploadCard";
 
 export default function Upload() {
   return (
     <div>
-      
       <h1 className="heading">Upload</h1>
       <UploadCard />
     </div>
-  )
+  );
 }
