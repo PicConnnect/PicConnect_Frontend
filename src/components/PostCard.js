91761615104fb877484343228fa7f0c2869e4da7
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-16 17:18:10 -0400

    fix: Formatting

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,38 +1,39 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-const PostCard = ({url}) => {
-      const navigate = useNavigate();
+const PostCard = ({ url }) => {
+  const navigate = useNavigate();
 
-      //allows to navigate to single
-      const handleViewClick = () => {
-        navigate(`/viewPost/`);//add variable postID
-      }
+  //allows to navigate to single
+  const handleViewClick = () => {
+    navigate(`/viewPost/`); //add variable postID
+  };
 
-
-    return (
-        <center>
-            <div>
-                <div className="card" style={{ width: '25rem' }}>
-                    <div className="cardButton" onClick={()=>{handleViewClick()}} >
-                        <img className="mainImage"
-                            src={url}
-                            // className="card-img-top"
-                            alt="..."
-                        />
-                        <div className="card-body">
-                            <p>Photo Title</p>
-                            {/* <p className="leftCentered">#Tags</p> */}
-
-                        </div>
-                    </div>
-                </div>
-
+  return (
+    <center>
+      <div>
+        <div className="card" style={{ width: "25rem" }}>
+          <div
+            className="cardButton"
+            onClick={() => {
+              handleViewClick();
+            }}
+          >
+            <img
+              className="mainImage"
+              src={url}
+              // className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <p>Photo Title</p>
+              {/* <p className="leftCentered">#Tags</p> */}
             </div>
-        </center>
-
-    );
+          </div>
+        </div>
+      </div>
+    </center>
+  );
 };
 
 export default PostCard;
-
