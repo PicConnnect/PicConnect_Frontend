de2316c5d43ddcce5a769473279ed79613afcecd
woodley <gelinw3@gmail.com>
2023-07-19 15:30:59 -0400

    refactor: remove upload header in upload page

--- src/Pages/Upload.js
+++ src/Pages/Upload.js
@@ -4,7 +4,6 @@ import UploadCard from "../components/UploadCard";
 export default function Upload() {
   return (
     <div style={{marginBottom: '5%'}}>
-      <h1 className="heading">Upload</h1>
       <UploadCard />
     </div>
   );
