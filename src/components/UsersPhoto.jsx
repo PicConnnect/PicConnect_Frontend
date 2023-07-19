41337e2e3a138eb0df4c45b3ade1ee78fcf0bf89
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-19 11:06:17 -0400

    feat: Show user's photos in the profile page

--- src/components/UsersPhoto.jsx
+++ src/components/UsersPhoto.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
-import PostCard from './PostCard'
-export default function UsersPhoto() {
+import React, { useEffect, useState } from "react";
+
+import PostCard from "./PostCard";
+
+export default function UsersPhoto({ userId }) {
+  const [photos, setPhotos] = useState([]);
+
+  //console.log("userId from useParams:", userId);
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await fetch(
+        `http://localhost:8000/api/photos/user/${userId}`
+      );
+      if (!response.ok) {
+        console.log('Response:', response);
+        throw new Error('Failed to fetch photos');
+    }
+      const data = await response.json();
+      setPhotos(data);
+    };
+    fetchPhotos();
+  }, [userId]);
+  console.log(photos);
   return (
-    <div className="bg-[#D9D9D9] mr-4" >
-        <h1 className="heading"> User's Photos</h1>
-          <div className="overflow-y-auto h-96">
-            <div className="flex flex-wrap justify-between">
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-              <PostCard url="https://images.pexels.com/photos/433989/pexels-photo-433989.jpeg?cs=srgb&dl=pexels-pixabay-433989.jpg&fm=jpg" size="small" />
-            </div>
-          </div>
+    <div className="bg-[#D9D9D9] mr-4">
+      <h1 className="heading"> User's Photos</h1>
+      <div className="overflow-y-auto h-96">
+        <div className="flex flex-wrap justify-between">
+          {photos.map((photo) => (
+            <PostCard
+              key={photo.id}
+              url={photo.urls}
+              size="small"
+              title ={photo.title}
+              postId={photo.id}
+            />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
