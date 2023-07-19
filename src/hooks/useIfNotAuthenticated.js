faaac715d38b5b0e96a3cf31c20ede7dad5a7b62
woodley <gelinw3@gmail.com>
2023-07-19 15:33:10 -0400

    feat: if user not logged check assign specific message to different pages

--- src/hooks/useIfNotAuthenticated.js
+++ src/hooks/useIfNotAuthenticated.js
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "./useAuth";
 
-export function useIfNotAuthenticated() {
+export function useIfNotAuthenticated(pageName) {
   const user = useAuth();
-
+  //check if user is not logged in and assign message to specific pages
   if (!user) {
     return (
       <div>
-        <p>You're not logged in! Please <Link to="/signin">Sign In</Link> or 
-        <Link to="/signup"> Sign Up</Link> to continue.</p>
+        {
+          pageName === "Following"? 
+          <div>
+            <h1 className="text-xl mb-5 mt-5 text-red-600">Couldn't See Following</h1>
+            <p>It looks like you are trying to see the people you follow.</p>
+            <p>To be able to view your following list you have to<Link to="/signin" className="text-blue-500"> Log In.</Link></p>
+            <p>If you don't have an account<Link to="/signup" className="text-blue-500"> Sign Up</Link> to Continue.</p>
+          </div>:
+          pageName === "Upload"?
+          <div>
+            <h1 className="text-xl mb-5 mt-5 text-red-600">Can't Upload Photo</h1>
+            <p>It looks like you are trying to upload a photo.</p>
+            <p>To be able to upload a photo you have to<Link to="/signin" className="text-blue-500"> Log In.</Link></p>
+            <p>If you don't have an account<Link to="/signup" className="text-blue-500"> Sign Up</Link> to Continue.</p>
+          </div>:
+          <div>
+            <h1 className="text-xl mb-5 mt-5 text-red-600">Can't Display Your Profile</h1>
+            <p>It looks like you are trying to see your profile.</p>
+            <p>To be able to view your profile you have to<Link to="/signin" className="text-blue-500"> Log In.</Link></p>
+            <p>If you don't have an account<Link to="/signup" className="text-blue-500"> Sign Up</Link> to Continue.</p>
+          </div>
+        }
       </div>
     );
   }
