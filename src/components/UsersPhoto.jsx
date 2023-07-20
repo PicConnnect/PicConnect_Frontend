d83b20996177de319bb71a481b78845fc7412281
Mohammed Fadel <mohammedfadel425@gmail.com>
2023-07-20 12:32:57 -0400

    Breaking Change: Added Followings, Followers components, Added Followings and Followers to Profile Page with css

--- src/components/UsersPhoto.jsx
+++ src/components/UsersPhoto.jsx
@@ -13,25 +13,26 @@ export default function UsersPhoto({ userId }) {
         `http://localhost:8000/api/photos/user/${userId}`
       );
       if (!response.ok) {
-        console.log('Response:', response);
-        throw new Error('Failed to fetch photos');
-    }
+        console.log("Response:", response);
+        throw new Error("Failed to fetch photos");
+      }
       const data = await response.json();
       setPhotos(data);
     };
     fetchPhotos();
   }, [userId]);
   console.log(photos);
-  return (    <div className="usersPhotos" >
-  <h1 className="heading"> User's Photos</h1>
-    <div className="miniPosts">
-      <div className="cards">
+  return (
+    <div className="usersPhotos">
+      <h1 className="heading"> User's Photos</h1>
+      <div className="miniPosts">
+        <div className="cards">
           {photos.map((photo) => (
             <PostCard
               key={photo.id}
               url={photo.urls}
               size="small"
-              title ={photo.title}
+              title={photo.title}
               postId={photo.id}
             />
           ))}
