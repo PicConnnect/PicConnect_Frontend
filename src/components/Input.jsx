a227b56ee537bc32076f1d7aff528e831d42b7a4
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-21 13:53:50 -0400

    refactor: Input elements for SignIn and SignUp Page

---
+++ src/components/Input.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Input ({ type, value, onChange, label, className }) {
+  return (
+    <div className={`w-full pt-6 ${className}`}>
+      <label htmlFor={label} className="block text-left font-bold">
+        {label}
+      </label>
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        required
+        className="w-full h-10"
+      />
+    </div>
+  );
+}
