98bea684e9a22a516cfd8b327a8ec5cbb5fd6f7b
woodley <gelinw3@gmail.com>
2023-07-27 14:00:01 -0400

    feat: retreive the poeople that the user is following ron database

--- src/components/Follows/Following/FollowingsList.jsx
+++ src/components/Follows/Following/FollowingsList.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleFollowingComponent from "./SingleFollowingComponent";
+import axios from "axios";
 
 const FollowingsList = () => {
+  const [followingList, setFollowingList] = useState([]);
+  const currentPath = window.location.pathname;
+  const userId = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+  const fetchFollowingList = () => axios.get(`http://localhost:8000/api/users/Following/${'firebase_user_id_1'}`).then((response) => {
+    if(response.status >= 200 && response.status < 300){
+      console.log('Request was successful!');
+      console.log('Response data:', response.data.following_id);
+      setFollowingList(response.data.following_id);
+    }else{
+      console.log('Request was not successful. Status code:', response.status);
+    }
+  }).catch((error) => {
+    console.error('An error occurred:', error.message);
+  })
+
+  useEffect(() => {
+    fetchFollowingList();
+    console.log("this is useeffec", followingList);
+  },[])
   return (
     <div className=" mb-2  items-center justify-center p-2 border-solid border-2 border-gray-700" style={{ height:"100%", width:"100%"}}>
       <h1 className="heading" style={{padding:"5px"}}>Following</h1>
       <div className="miniPosts" style={{height:"650px"}}>
-        <SingleFollowingComponent following={false} follower={true} />
-        <SingleFollowingComponent following={false} follower={true} />
-        <SingleFollowingComponent following={false} follower={true} />
-        <SingleFollowingComponent following={false} follower={true} />
+        {
+          followingList.map((usersFollowed) =>{
+            return <SingleFollowingComponent key={usersFollowed.id} data={usersFollowed}></SingleFollowingComponent>
+          })
+        }
       </div>
     </div>
   );
