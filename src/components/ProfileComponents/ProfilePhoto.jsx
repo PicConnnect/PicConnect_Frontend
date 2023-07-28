8ea0862f5cdbe92332e3f7ff114f7949c0299ab8
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-28 05:46:57 -0400

    style: Add Photo and Likes icons

--- src/components/ProfileComponents/ProfilePhoto.jsx
+++ src/components/ProfileComponents/ProfilePhoto.jsx
@@ -27,8 +27,7 @@ export default function ProfilePhoto() {
   const makeImageEditable = () => {
     setInitialImage(imageUrl);
     setIsEditingImage(true);
-  }
-
+  };
 
   const saveImage = async () => {
     setIsEditingImage(false);
@@ -94,13 +93,27 @@ export default function ProfilePhoto() {
     navigate(`/Follower/${auth.currentUser?.uid}`); // add variable userId
   };
 
+  const [textColorPhotos, setTextColorPhotos] = useState('text-gray-400'); // set initial color to greyish
+  const [textColorLikes, setTextColorLikes] = useState('text-gray-400'); // set initial color to greyish
+  const handleColorChangePhotos = () => {
+    setTextColorPhotos('text-black'); // change color to black on click
+  };
+
+  const handleColorChangeLikes = () => {
+    setTextColorLikes('text-black'); // change color to black on click
+  };
+
   return (
     <div className="userProfileContainer">
       <div>
         {isEditingImage ? (
           <div>
             <div className="w-48 h-48 overflow-hidden rounded-full mx-auto">
-              <img src={imageUrl} alt="User" className="object-cover w-full h-full" />
+              <img
+                src={imageUrl}
+                alt="User"
+                className="object-cover w-full h-full"
+              />
             </div>
             <div className="pt-6 flex items-center justify-center">
               <input
@@ -114,11 +127,28 @@ export default function ProfilePhoto() {
           </div>
         ) : (
           <div className="relative w-48 h-48 overflow-hidden rounded-full mx-auto">
-            <img src={imageUrl} alt="User" className="object-cover w-full h-full" />
+            <img
+              src={imageUrl}
+              alt="User"
+              className="object-cover w-full h-full"
+            />
             <div className="absolute inset-0 bg-black bg-opacity-40 flex items-center justify-center rounded-full opacity-0 hover:opacity-100 transition-opacity duration-500 ease-in-out">
-              <button onClick={makeImageEditable} className="p-2 bg-white bg-opacity-70 rounded-full">
-                <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+              <button
+                onClick={makeImageEditable}
+                className="p-2 bg-white bg-opacity-70 rounded-full"
+              >
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                  />
                 </svg>
               </button>
             </div>
@@ -126,10 +156,16 @@ export default function ProfilePhoto() {
         )}
         {isEditingImage ? (
           <div className="pt-6">
-            <button className="px-4 py-2 bg-green-500 text-white rounded-md" onClick={saveImage}>
+            <button
+              className="px-4 py-2 bg-green-500 text-white rounded-md"
+              onClick={saveImage}
+            >
               Save
             </button>
-            <button className="px-4 py-2 bg-red-500 text-white rounded-md ml-4" onClick={cancelImageEdit}>
+            <button
+              className="px-4 py-2 bg-red-500 text-white rounded-md ml-4"
+              onClick={cancelImageEdit}
+            >
               Cancel
             </button>
           </div>
@@ -149,6 +185,37 @@ export default function ProfilePhoto() {
         >
           Followers
         </button>
+      </div>
+      <div className="flex justify-center space-x-8 pt-6 ml-2 border-b border-gray-300">
+        <div className="flex items-center space-x-2 cursor-pointer border-b border-gray-300 onClick={handleColorChangePhotos}">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+          <span className={textColorPhotos}>Photos</span>
+        </div>
+        <div className="flex items-center space-x-2 cursor-pointer border-b border-gray-300" onClick={handleColorChangeLikes}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="black"
+          >
+            <path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z" />
+          </svg>
+          <span className="{textColorLikes}">Likes</span>
+        </div>
       </div>
     </div>
   );
