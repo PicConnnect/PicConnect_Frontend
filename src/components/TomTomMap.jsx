a6d9749da20c85421789267325eabbfc4c5636cc
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-27 23:19:08 -0400

    doc: clean up and commented

--- src/components/TomTomMap.jsx
+++ src/components/TomTomMap.jsx
@@ -5,26 +5,27 @@ import '@tomtom-international/web-sdk-maps/dist/maps.css'
 const TomTomMap = ({lat,lng}) => {
     console.log("lat ", lat , "lng", lng);
     const mapContainerRef = useRef(null);
-    const mapInstanceRef = useRef(null);
-    const markerRef = useRef(null);
 
     useEffect(() => {
+        //init map with api key
         const map = tt.map({
             key: "tvvS8ShGh7izeHqPcrjDnEpWxo0Y5P9P",
             container: mapContainerRef.current,
             center: [lng, lat],
             zoom: 15,
         });
-        mapInstanceRef.current = map;
+
+        //create a new marker with the specified coordinates and set/add it in map
         const marker = new tt.Marker({draggable: false}).setLngLat([lng, lat]);
         marker.addTo(map)
 
+        //clean up the map when unmount
         return() => {
             map.remove();
-            
         };
     }, [lat,lng]);
 
+    //render use ref to avoid unnecessary rerender
     return <div ref = {mapContainerRef} style={{ width: '100%', height: '500px' }}/>
 };
 export default TomTomMap
