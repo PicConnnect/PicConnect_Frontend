85e44897d6c12a67b79a2fce63f199229f804745
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-07-31 05:19:54 -0400

    feat: Conditionally render sign out or sign in in the navbar

--- src/components/ProfileComponents/SignInButtonComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function SignInButtonComponent({handleLogIn}) {
-    return (
-        <div>
-            <button className="bg-[#0000FF] mb-2 items-center justify-center p-2 border-solid rounded" onClick={handleLogIn}><span className="text-white">Sign Out</span></button>
-        </div>
-    )
-}
--- src/components/ProfileComponents/SignOutButtonComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function SignOutButtonComponent({handleLogout}) {
-    return (
-        <div>
-            <button className="bg-[#eb2b2b] mb-2 items-center justify-center p-2 border-solid rounded" onClick={handleLogout}><span className="text-white">Sign Out</span></button>
-        </div>
-    )
-}
